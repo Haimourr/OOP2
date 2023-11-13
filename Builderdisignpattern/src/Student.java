707d6443c75342fec9ca0d1828093bd932d00c97
Haimour_1 <91762357+Haimourr@users.noreply.github.com>
2023-11-13 18:26:31 +0300

    Update Student.java

--- Builderdisignpattern/src/Student.java
+++ Builderdisignpattern/src/Student.java
@@ -9,5 +9,79 @@
  * @author ammar
  */
 public class Student {
+        private int id;
+    private String  name;
+    private double gpa;
+    private String email;
+    
+    public Student(int id, String name, double gpa) {
+        
+         setId(id);
+         setName(name);
+         setGpa(gpa);
+    }
+     public Student( ){
+         
+     }
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    public Student WithId(int id ){
+        setId(id);
+        return this;
+    
+    }
+    
+    public Student WithName(String name ){
+        
+        setName(name);
+        return this;
+    }
+    
+    public Student WithGpa(double gpa ){
+        
+        setGpa(gpa);
+        return this;
+     
+    }
+    
+    public Student WithEmail(String email ){
+        
+        setEmail(email);
+        return this;
+    }
+    
+    
+    
     
 }
